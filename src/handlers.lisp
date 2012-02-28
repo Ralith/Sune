4f6dd1372ee73a3c6ccdf05d1db6391aa8449515
Benjamin Saunders <besaunde@sfu.ca>
2012-02-27 22:00:15 -0800

    Added nickchange tracking and combo participation.

--- src/handlers.lisp
+++ src/handlers.lisp
@@ -15,3 +15,29 @@
 (defhandler ping "PING" (c s params)
   (declare (ignore s))
   (enqueue-message c (concatenate 'string "PONG :" (first params))))
+
+(defhandler own-nick-track "NICK" (c sender params)
+  (when (string= (first sender) (connection-nick c))
+    (setf (connection-nick c) (first params))))
+
+(defun pm? (connection target)
+  (string= target (connection-nick connection)))
+
+;;; TODO: Per-connection state?
+(let ((state (make-hash-table :test 'equal)))
+ (defhandler combo "PRIVMSG" (c sender params)
+   (destructuring-bind (target message) params
+     (let ((reply-to (if (pm? c target) (first sender) target)))
+       (multiple-value-bind (last exists) (gethash reply-to state)
+         (cond
+           ((not exists)
+            (setf last (cons message 0))
+            (setf (gethash reply-to state) last))
+           ((string= (car last) message)
+            (incf (cdr last)))
+           (t
+            (setf (car last) message
+                  (cdr last) 0)))
+         (when (>= (cdr last) 2)
+           (enqueue-message c (format nil "PRIVMSG ~A :~A" reply-to message))
+           (setf (car last) nil)))))))
