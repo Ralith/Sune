fdb021bfc2cb95f6c40b52b2e3aade428b4469fb
Benjamin Saunders <besaunde@sfu.ca>
2012-03-01 23:46:52 -0800

    Async-write and string trimming utility code.

--- src/utils.lisp
+++ src/utils.lisp
@@ -5,3 +5,35 @@
     (setf (cdr acons) value)
     (push (cons key value) alist))
   alist)
+
+(defun async-write (base fd data)
+  (let ((cursor 0))
+   (set-io-handler base fd :write
+                   (lambda (fd event exception)
+                     (declare (ignore event))
+                     (if exception
+                         (progn (warn "Error during write")
+                                (remove-fd-handlers base fd :write t))
+                         (progn
+                           (incf cursor
+                                 (cffi:with-pointer-to-vector-data (ptr data)
+                                   (isys:write fd (cffi:make-pointer
+                                                   (+ (cffi:pointer-address ptr)
+                                                      cursor))
+                                               (- (length data) cursor))))
+                           (when (= cursor (length data))
+                             (remove-fd-handlers base fd :write t))))))))
+
+(defun trim-left (chars string)
+  (loop for idx from 0
+        for c across string
+        while (find c chars)
+        finally (return (displace-array string :offset idx))))
+
+(defun trim-right (chars string)
+  (loop for idx from (1- (length string)) downto 0
+        while (find (aref string idx) chars)
+        finally (return (displace-array string :dimensions (1+ idx)))))
+
+(defun trim (chars string)
+  (trim-right chars (trim-left chars string)))
